c575d945c32cdc69df5f747b6f4e8d10677214eb
Indunil1992 <indunil@adroitlogic.com>
2019-06-28 17:10:57 +0530

    j

--- projectbyindunil/lambda.js
+++ projectbyindunil/lambda.js
@@ -4,12 +4,12 @@ const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
 
     s3.getObject({
-        'Bucket': "as2-test-lahiru",
-        'Key': "12"
+        'Bucket': "indunil1",
+        'Key': "Screenshot (6).png"
     }).promise()
         .then(data => {
-            console.log(data);  
-            callback(null, "Successfully executed to enduser headdata" + {data});         // successful response
+            console.log(data);
+            callback(null, "Successfully executed to enduser headdata" + { data });         // successful response
             /*
             data = {
                 AcceptRanges: "bytes", 
@@ -24,11 +24,11 @@ exports.handler = function (event, context, callback) {
             */
         })
         .catch(err => {
-            console.log(err, err.stack); 
+            console.log(err, err.stack);
             callback(null, "Successfully executed to enduser catchead" + err); // an error occurred
         });
 
 
 
-   // callback(null, { "message": "Successfully executed to enduser 4long time" });
+    // callback(null, { "message": "Successfully executed to enduser 4long time" });
 }
